a2ce9de0b94f5cb4196545d17cc22184c0ea1531
tubyliec <antikruk@vivaldi.net>
2025-01-19 15:52:20 +0300

    feat: add 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const os = require('node:os');
+const { stdin, stdout } = process;
+const { exit } = process;
+
+const pathToFile = path.join(__dirname, 'text.txt');
+const output = fs.createWriteStream(pathToFile, { encoding: 'utf-8' });
+
+stdout.write('How are you?\n');
+
+stdin.on('data', (input) => {
+  if (
+    input.toString() === 'exit' + os.EOL ||
+    input.toString() === 'EXIT' + os.EOL
+  ) {
+    process.exit();
+  } else {
+    output.write(input);
+  }
+});
+
+process.on('SIGINT', () => process.exit());
+process.on('exit', () => stdout.write(' See you again! '));
